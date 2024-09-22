1edce46c2d3774e12e1e7867c9e3fb8365c99307
Ian Buchanan <get.ian.buchanan@gmail.com>
2024-09-22 11:39:33 -0700

    add SeedColors.js & Palette.js

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className='Palette'>
+        {/* nav */}
+        <div className='Palette-colors'>{/* color boxes*/}</div>
+        {/* footer */}
+      </div>
+    );
+  }
+}
