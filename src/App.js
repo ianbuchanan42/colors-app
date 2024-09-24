4dad465cdb6f3f50b084340be4d7255f52b5e00e
Ian Buchanan <get.ian.buchanan@gmail.com>
2024-09-24 12:43:53 -0700

    add BrowserRouter Routes and Route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
-import Palette from './Palette';
-import SeedColors from './seedColors';
-import { generatePalette } from './colorHelper';
+import { Route, Routes } from 'react-router-dom';
+//import Palette from './Palette';
+//import SeedColors from './seedColors';
+//import { generatePalette } from './colorHelper';
 
 function App() {
-  console.log(generatePalette(SeedColors[4]));
   return (
-    <div>
-      <Palette palette={generatePalette(SeedColors[4])} />
-    </div>
+    <Routes>
+      <Route exact path='/' element={<h1>test</h1>} />
+      <Route exact path='/palette/:id' element={<h1>test 2</h1>} />
+    </Routes>
+
+    // <div>
+    //   <Palette palette={generatePalette(SeedColors[4])} />
+    // </div>
   );
 }
 
